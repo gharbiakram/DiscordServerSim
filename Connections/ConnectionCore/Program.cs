6f0367ae8091f479a6f222adf4fad2d89e9fc8e1
Akram Gharbi <akramgharbi13@gmail.com>
2025-01-24 17:19:01 +0100

    Added a basic implemntation for the server

---
+++ Connections/ConnectionCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConnectionCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new ServerCore();
+        }
+    }
+}
---
+++ Connections/ConnectionCore/ServerCore.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionCore
+{
+    public class ServerCore
+    {
+
+
+        private TcpListener _tcpListener;
+
+        private byte[] buffer;
+
+        public ServerCore()
+        {
+
+            ServerStart();
+
+        }
+
+
+        private void ServerStart()
+        {
+
+            IPAddress adrIP = IPAddress.Parse("127.0.0.1");
+            var Port = 13000;
+            _tcpListener = new(adrIP, Port);
+            _tcpListener.Start();
+
+            Console.WriteLine("Server Started");
+
+            buffer = new byte[256];
+
+            using TcpClient _tcpClient = _tcpListener.AcceptTcpClient();
+
+            Console.WriteLine("Client Connected");
+
+            var clientStream = _tcpClient.GetStream();
+
+            int totalReadCharFromStream;
+
+            while (( totalReadCharFromStream = clientStream.Read(buffer,0,buffer.Length)) != 0 ){
+
+
+                var IncomingMessage=Encoding.UTF8.GetString(buffer, 0, totalReadCharFromStream);
+
+                Console.WriteLine(IncomingMessage);
+
+
+
+            }
+
+            
+
+
+
+
+
+
+
+        }
+    
+
+
+
+
+    }
+}
