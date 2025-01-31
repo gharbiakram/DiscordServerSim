cb248ddf368ee7b4c544481080074b7881bea2dc
Akram Gharbi <akramgharbi13@gmail.com>
2025-01-31 01:48:42 +0100

    Made a remarkable advancement in making the UI

--- DiscordServerSimUI/MainWindow.xaml.cs
+++ DiscordServerSimUI/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DiscordServerSimUI
 {
@@ -16,11 +9,98 @@ namespace DiscordServerSimUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        //notifies the UI when changes occur , so it will be updatable
+        ObservableCollection<Message> Messages { get; set; } = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //link the Message List component with the message collection
+            MessageList.ItemsSource = Messages;
         }
 
-       
+        private void MessageBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+
+                SendMessage();
+                e.Handled = true;
+
+            }
+        }
+
+        private void SendingButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void SendMessage()
+        {
+
+            if (!string.IsNullOrWhiteSpace(MessageBox.Text))
+            {
+
+             Messages.Add(new Message { messageText = MessageBox.Text, Sender = new User("Akram") });
+
+            
+
+             MessageBox.Clear();
+
+
+
+             ScrollToBottom();
+
+
+            }
+
+
+        }
+
+        private void ScrollToBottom()
+        {
+            if(Messages.Count > 0)
+            {
+
+                MessageList.UpdateLayout();
+
+                MessageList.ScrollIntoView(Messages.Last());
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+    class User
+    {
+        public string Username { get; set; } // get and set so it will be bindable
+
+        public User(string username)
+        {
+
+
+            this.Username = username;
+
+
+
+        }
+
+
+    }
+    class Message
+    {
+        public User Sender { get; set; } // get and set so it will be bindable
+        public string messageText { get; set; }// get and set so it will be bindable
+
     }
 }
